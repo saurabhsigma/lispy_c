0d8343383844e7de06d2daa8e4808ced4527c306
saurabhsigma <saurabh962003@gmail.com>
2024-08-16 14:45:06 +0530

    resume

---
+++ basics.c
@@ -0,0 +1,67 @@
+
+// #include <stdio.h>
+// int add_together(int x, int y){
+//     return (x+y);
+// }
+// int main(int argc, char** argv){
+//     // puts("Hello world!");
+//     // return 0;
+
+
+//     int count = 10;
+//     int added = add_together(54,54);
+//     printf("%d\n", added);
+//     return 0;
+// }
+
+
+//  sturcture declarations 
+
+// typedef struct  
+// {
+//     /* data */
+//     float x;
+//     float y;
+// } point ;
+
+// point p;
+// p.x = 0.1;
+// p.y = 10.0;
+
+//  float length = sqrt(p.x * p.x + p.y * p.y);
+// }
+
+// conditionals
+
+// if (x > 10 && x < 100) {
+//   puts("x is greater than 10 and less than 100!");
+// } else {
+//   puts("x is less than 11 or greater than 99!");
+// }
+
+// loops 
+
+// while
+// int i = 10;
+// while (i > 0) {
+//   puts("Loop Iteration");
+//   i = i - 1;
+// }
+
+// for
+// for (int i = 0; i < 10; i++) {
+//   puts("Loop Iteration");
+// }
+
+// › What does the typedef keyword do exactly?
+
+// The typedef keyword in C and C++ is used to create an alias or a synonym for an existing data type. 
+// It allows you to define a new name for an existing data type that you can then use interchangeably 
+// with the original type name. This is particularly useful for improving code readability, abstraction, and portability.
+
+// typedef struct person Person;
+
+
+
+// After this typedef declaration, you can use Person instead of struct person to declare variables:
+
